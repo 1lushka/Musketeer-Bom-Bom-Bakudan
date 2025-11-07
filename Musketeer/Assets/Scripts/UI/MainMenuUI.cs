640d378970d63b39ad9b0e9b937e5cee8a5ff2af
Nikitoti-k <r7e7d7@yandex.ru>
2025-11-07 23:00:03 +0300

    Добавил MainMenuUI
    Финкционал для главного меню в общем-то + заготовка для него. + добавил TMP

---
+++ Musketeer/Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [Header("Кнопки")]
+    public Button startButton;
+    public Button settingsButton;
+    public Button exitButton;
+
+    [Header("Панели")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    private void Start()
+    {
+        startButton?.onClick.AddListener(StartGame);
+        settingsButton?.onClick.AddListener(OpenSettings);
+        exitButton?.onClick.AddListener(ExitGame);
+
+        ShowMainMenu();
+    }
+
+    void StartGame()
+    {
+        LoadGameWithFade();
+    }
+
+    void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    void ExitGame()
+    { 
+        GameManager.Instance.QuitGame();
+    }
+
+    void LoadGameWithFade()
+    {
+        GameManager.Instance.LoadLevel("GameScene"); 
+    }
+
+    void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+}
