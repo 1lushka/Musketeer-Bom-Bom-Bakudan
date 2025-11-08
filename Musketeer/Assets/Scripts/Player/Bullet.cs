903409d321a7d4ca04f23e6b3c54d6b8ff92a245
1lushka <il.zah@mail.ru>
2025-11-08 18:41:59 +0300

    Добавил bullet

--- Musketeer/Assets/Scripts/Player/Bullet.cs
+++ Musketeer/Assets/Scripts/Player/Bullet.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Параметры снаряда")]
+    public float speed = 20f;
+    public float damage = 10f;
+    public float lifetime = 5f;
+
+    private Vector3 targetPosition;
+    private float spawnHeight;
+
+    public void Shoot(Vector3 target)
     {
-        
+        spawnHeight = transform.position.y;
+        targetPosition = new Vector3(target.x, spawnHeight, target.z);
+        gameObject.SetActive(true);
+        CancelInvoke();
+        Invoke(nameof(Deactivate), lifetime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyAbstract enemy = other.GetComponent<EnemyAbstract>();
+        if (enemy == null) enemy = other.GetComponentInParent<EnemyAbstract>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+            Deactivate();
+        }
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
